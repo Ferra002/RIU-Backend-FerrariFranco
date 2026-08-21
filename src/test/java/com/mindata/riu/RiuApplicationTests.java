2cfe270236bfdd908b17fefbc5ee23f36ba48ce3
Franco Ferrari <franconferrari02@outlook.com>
2026-08-21 13:55:35 -0300

    First commit

---
+++ src/test/java/com/mindata/riu/RiuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindata.riu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
