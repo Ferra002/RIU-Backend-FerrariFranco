78f2f3f99eef1749dc63791fd4203858c4737327
Franco Ferrari <franconferrari02@outlook.com>
2026-08-24 21:51:21 -0300

    Add K6 script

---
+++ k6/search-tests.js
@@ -0,0 +1,32 @@
+import http from 'k6/http'
+import { check } from 'k6'
+
+export const options = {
+  vus: 100,
+  duration: '30s',
+}
+
+export default function () {
+    const payload = JSON.stringify({
+        hotelId: '1234aBc',
+        checkIn: '2026-08-24',
+        checkOut: '2026-09-24',
+        ages: [59, 54, 24]
+    });
+
+    const response = http.post(
+        'http://localhost:3000/api/hotel-search/v1/search',
+        payload,
+        {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+    );
+
+    check(response, {
+        'status is 201': (r) => r.status === 201,
+        'has searchId': (r) => r.json('searchId') != null
+    });
+
+}
