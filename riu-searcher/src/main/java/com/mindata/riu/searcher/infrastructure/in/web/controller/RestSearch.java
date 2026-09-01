d8dd16201d4fa87c15828732aff01dd52dd18f7e
Franco Ferrari <franconferrari02@outlook.com>
2026-08-31 22:12:28 -0300

    Remove unused import

--- riu-searcher/src/main/java/com/mindata/riu/searcher/infrastructure/in/web/controller/RestSearch.java
+++ riu-searcher/src/main/java/com/mindata/riu/searcher/infrastructure/in/web/controller/RestSearch.java
@@ -9,7 +9,6 @@ import com.mindata.riu.searcher.infrastructure.in.web.mapper.CountMapper;
 import com.mindata.riu.searcher.infrastructure.in.web.mapper.SearchMapper;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
